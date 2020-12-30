f4f571ff7568730c761abb83b618459fd2f1d4fd
Tony Messias <tonysm@hey.com>
2020-12-29 23:48:26 -0300

    Document the queue config option

--- config/turbo-laravel.php
+++ config/turbo-laravel.php
@@ -1,5 +1,16 @@
 <?php
 
 return [
+    /*
+    |--------------------------------------------------------------------------
+    | Queue Data Broadcasting
+    |--------------------------------------------------------------------------
+    |
+    | This option allows you to control if the broadcast updates to your frontend
+    | will be queued or not. When this is set to "true" then all the broadcast
+    | operations will be queued for a better performance of your main code.
+    |
+    */
+
     'queue' => true,
 ];
