69d5dacaea499d1daac0200b2757679173be9464
Tony Messias <tonysm@hey.com>
2020-12-29 23:50:06 -0300

    Add docblocks for the broadcasts trait

--- src/Models/ModelObserver.php
+++ src/Models/ModelObserver.php
@@ -6,11 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class ModelObserver
 {
+    /**
+     * @param Model|Broadcasts $model
+     */
     public function saved(Model $model)
     {
         $model->queueBroadcastToHotwire();
     }
 
+    /**
+     * @param Model|Broadcasts $model
+     */
     public function deleted(Model $model)
     {
         $model->queueBroadcastRemovalToHotwire();
