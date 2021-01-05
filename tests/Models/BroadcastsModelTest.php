5d99b80a165c1f8baad5f72bd08d0d60bd0dd1fd
Tony Messias <tonysm@hey.com>
2021-01-05 00:49:54 -0300

    Adds test cases for the brodcasts model trait

---
+++ tests/Models/BroadcastsModelTest.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Tonysm\TurboLaravel\Tests\Models;
+
+use Tonysm\TurboLaravel\Tests\TestCase;
+
+class BroadcastsModelTest extends TestCase
+{
+    /** @test */
+    public function broadcasts_on_create()
+    {
+    }
+
+    /** @test */
+    public function broadcasts_on_update()
+    {
+    }
+
+    /** @test */
+    public function broadcasts_on_delete()
+    {
+    }
+
+    /** @test */
+    public function broadcasts_on_soft_deleted()
+    {
+    }
+
+    /** @test */
+    public function broadcasts_on_hard_delete_using_soft_deletes_trait()
+    {
+    }
+
+    /** @test */
+    public function broadcasts_using_override_action()
+    {
+    }
+
+    /** @test */
+    public function broadcasts_using_override_target_id()
+    {
+    }
+
+    /** @test */
+    public function broadcasts_to_related_model_using_override_property()
+    {
+    }
+
+    /** @test */
+    public function broadcasts_to_related_model_using_override_method()
+    {
+    }
+
+    /** @test */
+    public function broadcasts_using_another_channel()
+    {
+    }
+
+    /** @test */
+    public function broadcasts_using_override_partial_name()
+    {
+    }
+
+    /** @test */
+    public function broadcasts_using_override_partial_data()
+    {
+    }
+}
