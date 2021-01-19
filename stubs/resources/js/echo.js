faa447ca968c778d8a06efed102a57929dca01af
Tony Messias <tonysm@hey.com>
2021-01-19 00:16:27 -0300

    Add missing configs to echo.js stub

--- stubs/resources/js/echo.js
+++ stubs/resources/js/echo.js
@@ -6,7 +6,10 @@ const echo = new Echo({
     broadcaster: 'pusher',
     key: process.env.MIX_PUSHER_APP_KEY,
     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
-    forceTLS: true
+    forceTLS: process.env.MIX_PUSHER_USE_SSL === "true",
+    disableStats: true,
+    wsHost: process.env.MIX_PUSHER_HOST,
+    wsPort: process.env.MIX_PUSHER_PORT || null,
 });
 
 export default echo;
