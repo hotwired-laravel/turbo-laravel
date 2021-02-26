e85955fae40192f604486cb582698fefd3b314c2
Tony Messias <tonyzrp@gmail.com>
2021-02-25 23:02:43 -0300

    Update app.js scaffolding to import Turbo differently

--- stubs/resources/js/app.js
+++ stubs/resources/js/app.js
@@ -1,9 +1,7 @@
-import {
-    start as startTurbo
-} from '@hotwired/turbo';
+import * as Turbo from '@hotwired/turbo';
 import('./bootstrap');
 import('./elements/turbo-echo-stream-tag');
 //=inject-alpine
 //=inject-stimulus
 
-startTurbo();
+Turbo.start();
