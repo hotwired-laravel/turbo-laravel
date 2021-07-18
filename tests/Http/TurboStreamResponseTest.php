dfcccb817a571d2fb9a6c5e30de1d3a460bd80ff
Tony Messias <tonysm@hey.com>
2021-07-18 20:20:35 -0300

    Adds the InteractsWithTurbo testing helper trait

--- tests/Http/TurboStreamResponseTest.php
+++ tests/Http/TurboStreamResponseTest.php
@@ -4,10 +4,13 @@ namespace Tonysm\TurboLaravel\Tests\Http;
 
 use Illuminate\Support\Facades\View;
 use Tonysm\TurboLaravel\Testing\AssertableTurboStream;
+use Tonysm\TurboLaravel\Testing\InteractsWithTurbo;
 use Tonysm\TurboLaravel\Tests\TestCase;
 
 class TurboStreamResponseTest extends TestCase
 {
+    use InteractsWithTurbo;
+
     public function setUp(): void
     {
         parent::setUp();
@@ -18,28 +21,34 @@ class TurboStreamResponseTest extends TestCase
     protected function defineRoutes($router)
     {
         $router->get('/testing/turbo-stream', function () {
-            return response()->turboStreamView('turbo_streams');
-        })->name('testing.turbo-stream');
+            if (request()->wantsTurboStream()) {
+                return response()->turboStreamView('turbo_streams');
+            }
 
-        $router->get('/testing/non-turbo-stream', function () {
             return 'No Turbo Stream';
-        })->name('testing.non-turbo-stream');
+        })->name('testing.turbo-stream');
     }
 
     /** @test */
     public function turbo_stream_response()
     {
-        $this->get(route('testing.turbo-stream'))
+        $this->turbo()
+            ->get(route('testing.turbo-stream'))
             ->assertTurboStream();
+    }
 
-        $this->get(route('testing.non-turbo-stream'))
+    /** @test */
+    public function not_turbo_response()
+    {
+        $this->get(route('testing.turbo-stream'))
             ->assertNotTurboStream();
     }
 
     /** @test */
     public function turbo_assert_count_of_turbo_streams()
     {
-        $this->get(route('testing.turbo-stream'))
+        $this->turbo()
+            ->get(route('testing.turbo-stream'))
             ->assertTurboStream(fn (AssertableTurboStream $turboStream) => (
                 $turboStream->has(3)
             ));
@@ -48,7 +57,8 @@ class TurboStreamResponseTest extends TestCase
     /** @test */
     public function turbo_assert_has_turbo_stream()
     {
-        $this->get(route('testing.turbo-stream'))
+        $this->turbo()
+            ->get(route('testing.turbo-stream'))
             ->assertTurboStream(fn (AssertableTurboStream $turboStreams) => (
                 $turboStreams->has(3)
                 && $turboStreams->hasTurboStream(fn ($turboStream) => (
