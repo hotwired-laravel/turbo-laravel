a854d7e83c190dfaa5397879d8b5cfd11a4cd439
Tony Messias <tonysm@hey.com>
2021-09-24 14:30:46 -0300

    Fix controllers import

--- stubs/resources/js/libs/stimulus.js
+++ stubs/resources/js/libs/stimulus.js
@@ -2,5 +2,5 @@ import { Application } from '@hotwired/stimulus'
 import { definitionsFromContext } from '@hotwired/stimulus-webpack-helpers'
 
 window.Stimulus = Application.start();
-const context = require.context("./controllers", true, /\.js$/);
+const context = require.context("../controllers", true, /\.js$/);
 Stimulus.load(definitionsFromContext(context));
