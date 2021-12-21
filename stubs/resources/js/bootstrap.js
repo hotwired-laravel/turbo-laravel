078c5d6f703d8569cc6eeffd2259b33ccddb7636
Tony Messias <tonysm@hey.com>
2021-12-20 23:13:29 -0300

    Comment out the Laravel Echo setup

--- stubs/resources/js/bootstrap.js
+++ stubs/resources/js/bootstrap.js
@@ -10,20 +10,44 @@ window.axios = require('axios');
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
-import Echo from 'laravel-echo';
+/**
+ * The Laravel Echo setup is commented out because it doesn't work out of the box
+ * as you will need to setup a WebSocket service to be able to use it. Once that's
+ * handled, you may uncomment the following lines to start receiving WebSockets.
+ *
+ * See: https://github.com/tonysm/turbo-laravel/#broadcasting-turbo-streams-over-websockets-with-laravel-echo
+ */
 
-window.Pusher = require('pusher-js');
+// import Echo from 'laravel-echo';
 
-window.Echo = new Echo({
-    broadcaster: 'pusher',
-    key: process.env.MIX_PUSHER_APP_KEY,
-    cluster: process.env.MIX_PUSHER_APP_CLUSTER,
-    forceTLS: process.env.MIX_PUSHER_APP_USE_SSL === "true",
-    disableStats: true,
-    wsHost: process.env.MIX_PUSHER_APP_HOST,
-    wsPort: process.env.MIX_PUSHER_APP_PORT || null,
-});
+// window.Pusher = require('pusher-js');
+
+// window.Echo = new Echo({
+//     broadcaster: 'pusher',
+//     key: process.env.MIX_PUSHER_APP_KEY,
+//     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
+//     forceTLS: process.env.MIX_PUSHER_APP_USE_SSL === "true",
+//     disableStats: true,
+//     wsHost: process.env.MIX_PUSHER_APP_HOST,
+//     wsPort: process.env.MIX_PUSHER_APP_PORT || null,
+// });
+
+// document.addEventListener('turbo:before-fetch-request', (e) => {
+//     e.detail.fetchOptions.headers['X-Socket-ID'] = window.Echo.socketId();
+// });
+
+/**
+ * Turbo turns forms and links into AJAX requests using `fetch`. Non-GET form requests in Laravel need a special kind of value called
+ * "The CSRF Token" to be allowed to enter your app. When rendering forms in background, such as when you're broadcasting HTML to
+ * all users which contain a form on it, we don't have the receiving users' tokens, so we need to add it to the fetch request.
+ */
+
+let token = document.head.querySelector('meta[name="csrf-token"]');
 
 document.addEventListener('turbo:before-fetch-request', (e) => {
-    e.detail.fetchOptions.headers['X-Socket-ID'] = window.Echo.socketId();
+    if (token) {
+        e.detail.fetchOptions.headers['X-CSRF-Token'] = token.content;
+    } else {
+        console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
+    }
 });
