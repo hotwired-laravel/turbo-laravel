daa2bdd305f785594f6dad00c6ddaeee821a4f76
Tony Messias <tonysm@hey.com>
2022-08-19 18:42:22 -0300

    Tweaks the constructor arguments position for target and targets

--- src/Views/Components/Stream.php
+++ src/Views/Components/Stream.php
@@ -9,34 +9,31 @@ use function Tonysm\TurboLaravel\dom_id;
 
 class Stream extends Component
 {
-    public string|Model|array|null $target;
+    public string|Model|array|null $target = null;
+    public string|null $targets = null;
 
     public ?string $action;
-
-    public ?string $targets;
 
     /**
      * Create a new component instance.
      *
-     * @param string|Model|array|null $target The DOM ID string, a model to generate the DOM ID for, or an array to be passed to the `dom_id` function.
      * @param ?string $action One of the seven Turbo Stream actions: "append", "prepend", "before", "after", "replace", "update", or "remove".
+     * @param string|Model|array|null $target The DOM ID string, a model to generate the DOM ID for, or an array to be passed to the `dom_id` function.
      * @param string|null $targets The CSS selector to apply the action to multiple targets
      */
-    public function __construct(string|Model|array|null $target = null, ?string $action = null, ?string $targets = null)
+    public function __construct(string $action, string|Model|array|null $target = null, string|null $targets = null)
     {
-        $this->target = $target;
-        $this->action = $action;
-        $this->targets = $targets;
-        if (!$action) {
-            throw new \InvalidArgumentException('Action is required');
-        }
-        if (!$target && !$targets) {
-            throw new \InvalidArgumentException('targets and target cannot be both null');
+        if (! $target && ! $targets) {
+            throw new \InvalidArgumentException('No target was specified');
         }
 
         if ($target && $targets) {
-            throw new \InvalidArgumentException('targets and target are both set. One needs to be null');
+            throw new \InvalidArgumentException('Must specify either target or targets attributes, but never both.');
         }
+
+        $this->target = $target;
+        $this->targets = $targets;
+        $this->action = $action;
     }
 
     /**
@@ -48,12 +45,13 @@ class Stream extends Component
     {
         return view('turbo-laravel::components.stream', [
             'targetValue' => $this->targetValue(),
+            'targetTag' => ($this->targets ?? false) ? 'targets' : 'target',
         ]);
     }
 
     private function targetValue(): string
     {
-        if (isset($this->targets)){
+        if ($this->targets ?? false) {
             return $this->targets;
         }
 
