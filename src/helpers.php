825aadc04e86f4fce14b4a51bf173f3113353554
Tony Messias <tonysm@hey.com>
2022-09-06 21:58:24 -0300

    Only register the namespaced functions if they are not already registered

--- src/helpers.php
+++ src/helpers.php
@@ -4,27 +4,31 @@ namespace Tonysm\TurboLaravel;
 
 use Tonysm\TurboLaravel\Views\RecordIdentifier;
 
-/**
- * Generates the DOM ID for a specific model.
- *
- * @param object $model
- * @param string $prefix
- *
- * @return string
- */
-function dom_id(object $model, string $prefix = ""): string
-{
-    return (new RecordIdentifier($model))->domId($prefix);
+if (! function_exists('dom_id')) {
+    /**
+     * Generates the DOM ID for a specific model.
+     *
+     * @param object $model
+     * @param string $prefix
+     *
+     * @return string
+     */
+    function dom_id(object $model, string $prefix = ""): string
+    {
+        return (new RecordIdentifier($model))->domId($prefix);
+    }
 }
 
-/**
- * Generates the DOM CSS Class for a specific model.
- *
- * @param object $model
- * @param string $prefix
- * @return string
- */
-function dom_class(object $model, string $prefix = ""): string
-{
-    return (new RecordIdentifier($model))->domClass($prefix);
+if (! function_exists('dom_class')) {
+    /**
+     * Generates the DOM CSS Class for a specific model.
+     *
+     * @param object $model
+     * @param string $prefix
+     * @return string
+     */
+    function dom_class(object $model, string $prefix = ""): string
+    {
+        return (new RecordIdentifier($model))->domClass($prefix);
+    }
 }
