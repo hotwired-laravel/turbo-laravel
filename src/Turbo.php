b8c82dc35a03d45700ac4cae76e87b0f8833603d
Tony Messias <tonysm@hey.com>
2023-03-28 22:31:40 -0300

    Remove unused code

--- src/Turbo.php
+++ src/Turbo.php
@@ -24,13 +24,6 @@ class Turbo
      */
     private bool $broadcastToOthersOnly = false;
 
-    /**
-     * The URIs of
-     *
-     * @var array
-     */
-    private array $routeGuessingExceptions = [];
-
     public function isTurboNativeVisit(): bool
     {
         return $this->visitFromTurboNative;
