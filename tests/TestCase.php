26c13ca7099e46cdc3feea54a2cdbcdab612ecab
Mior Muhammad Zaki <crynobone@gmail.com>
2023-09-09 09:46:21 +0800

    Workbench Improvements
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,29 +4,24 @@ namespace HotwiredLaravel\TurboLaravel\Tests;
 
 use HotwiredLaravel\TurboLaravel\Facades\TurboStream;
 use HotwiredLaravel\TurboLaravel\TurboServiceProvider;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Orchestra\Testbench\Concerns\WithWorkbench;
 use Orchestra\Testbench\TestCase as Orchestra;
 use Workbench\App\Providers\WorkbenchAppServiceProvider;
 
 class TestCase extends Orchestra
 {
+    use RefreshDatabase;
+    use WithWorkbench;
+
     protected function setUp(): void
     {
         parent::setUp();
 
-        $this->loadMigrationsFrom(dirname(__DIR__).'/workbench/database/migrations/');
-
         TurboStream::fake();
     }
 
-    protected function getPackageProviders($app)
-    {
-        return [
-            TurboServiceProvider::class,
-            WorkbenchAppServiceProvider::class,
-        ];
-    }
-
-    public function getEnvironmentSetUp($app)
+    protected function defineEnvironment($app)
     {
         $app['config']->set('database.default', 'sqlite');
 
