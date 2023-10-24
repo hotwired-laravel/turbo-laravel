305a9d54fe2d3dc2ce163c2a6279eaef828b5d6e
Tony Messias <tonysm@hey.com>
2023-10-24 00:48:44 -0300

    Adds a fromTurboFrame() testing helper

--- src/Testing/InteractsWithTurbo.php
+++ src/Testing/InteractsWithTurbo.php
@@ -18,4 +18,9 @@ trait InteractsWithTurbo
     {
         return $this->withHeader('User-Agent', 'Turbo Native Android; Mozilla: Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.3 Mozilla/5.0 (Macintosh; Intel Mac OS X x.y; rv:42.0) Gecko/20100101 Firefox/43.4');
     }
+
+    public function fromTurboFrame(string $frame): self
+    {
+        return $this->withHeader('Turbo-Frame', $frame);
+    }
 }
