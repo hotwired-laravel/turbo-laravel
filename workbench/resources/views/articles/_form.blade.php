347eb2cd10c2c919f767cf33df3b0b2ffa782f14
Tony Messias <tonysm@hey.com>
2023-10-29 22:55:08 -0300

    Fix input styles in darkmode

--- workbench/resources/views/articles/_form.blade.php
+++ workbench/resources/views/articles/_form.blade.php
@@ -9,18 +9,18 @@
     @endif
 
     <div>
-        <label class="block font-medium text-sm text-gray-700 dark:text-gray-300" for="title">{{ __('Title') }}</label>
-        <input class="w-full border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm" type="text" name="title" placeholder="{{ __('Title') }}" autofocus value="{{ old('title', $article?->title) }}" autocomplete="off" />
+        <label class="block font-medium text-sm text-gray-700" for="title">{{ __('Title') }}</label>
+        <input class="w-full border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm" type="text" name="title" placeholder="{{ __('Title') }}" autofocus value="{{ old('title', $article?->title) }}" autocomplete="off" />
         @error('title')
-        <span class="mt-1 block text-sm text-red-600 dark:text-red-400">{{ $message }}</span>
+        <span class="mt-1 block text-sm text-red-600">{{ $message }}</span>
         @enderror
     </div>
 
     <div class="mt-2">
-        <label class="block font-medium text-sm text-gray-700 dark:text-gray-300" for="content">{{ __('Content') }}</label>
-        <textarea class="w-full border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm" name="content" id="content" cols="30" rows="10" autocomplete="off" placeholder="{{ __('Say something...') }}">{{ old('content', $article?->content) }}</textarea>
+        <label class="block font-medium text-sm text-gray-700" for="content">{{ __('Content') }}</label>
+        <textarea class="w-full border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm" name="content" id="content" cols="30" rows="10" autocomplete="off" placeholder="{{ __('Say something...') }}">{{ old('content', $article?->content) }}</textarea>
         @error('content')
-        <span class="mt-1 block text-sm text-red-600 dark:text-red-400">{{ $message }}</span>
+        <span class="mt-1 block text-sm text-red-600">{{ $message }}</span>
         @enderror
     </div>
 
