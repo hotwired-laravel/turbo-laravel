90735b10dbdb850c1a3409cc4fe6900302eb22e5
Tony Messias <tonysm@hey.com>
2023-10-30 00:25:19 -0300

    Extracts the query string from fallback url and sends to native routes too

--- src/Http/TurboNativeRedirectResponse.php
+++ src/Http/TurboNativeRedirectResponse.php
@@ -7,12 +7,27 @@ use Illuminate\Support\Str;
 
 class TurboNativeRedirectResponse extends RedirectResponse
 {
+    public static function createFromFallbackUrl(string $action, string $fallbackUrl)
+    {
+        return (new self(route("turbo_{$action}_historical_location")))
+            ->withQueryString((new self($fallbackUrl))->getQueryString());
+    }
+
     public function with($key, $value = null)
     {
         $params = $this->getQueryString();
 
         return $this->withoutQueryStrings()
             ->setTargetUrl($this->getTargetUrl().'?'.http_build_query($params + [$key => urlencode($value)]));
+    }
+
+    protected function withQueryString(array $params): self
+    {
+        foreach ($params as $key => $val) {
+            $this->with($key, $val);
+        }
+
+        return $this;
     }
 
     protected function getQueryString(): array
